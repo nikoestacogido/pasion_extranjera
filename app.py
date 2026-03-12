f187ab9831720a7705fcec29e89c49966efec379
nikoestacogido <106939862+nikoestacogido@users.noreply.github.com>
2026-03-12 19:19:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
+app = FastAPI()
+
+templates = Jinja2Templates(directory = "templates")
+
+@app.get("/", response_class = HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
