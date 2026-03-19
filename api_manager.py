23738753d2fb133302fda857557ad15a78232c43
nikoestacogido <106939862+nikoestacogido@users.noreply.github.com>
2026-03-19 14:40:24 -0300

    Update api_manager.py

--- api_manager.py
+++ api_manager.py
@@ -1,9 +1,9 @@
 # API KEY 507e730cb209ad0f97228d13ee84d748
 # Aca manejo todo lo que se comunica con la API, y de aca salen funciones corte getmatches()
 import time
-import requests
 import os
-from database_func import getTeamsFeature
+import requests
+from database_func import getTeamsFeature, getTeamFeature
 
 API_KEY = os.environ["FOOTBALL_API_KEY"]
 headers = { "X-Auth-Token": API_KEY }
@@ -12,12 +12,21 @@ params = {
     "limit": 1
 }
 
-def getMatches(): # All matches envolving the few teams
-    ids = getTeamsFeature("id")
+def getMatches(ids): # All matches envolving the few teams
+    matches = []
     for team in ids:
-        time.sleep(10)
+        match = []
+        time.sleep(5)
         id = team
         url = f"https://api.football-data.org/v4/teams/{id}/matches"
         response = requests.get(url, headers = headers, params = params)
         data = response.json()
-        print(f"{data["matches"][0]["homeTeam"]["name"]} vs {data["matches"][0]["awayTeam"]["name"]}")
+        home = True
+        if data["matches"][0]["awayTeam"]["name"] == getTeamFeature("name", id)[0]: home = False
+        match.append(id)
+        match.append(data["matches"][0]["utcDate"])
+        match.append(data["matches"][0]["homeTeam"]["name"])
+        match.append(data["matches"][0]["awayTeam"]["name"])
+        match.append(home)
+        matches.append(match)
+    return matches
